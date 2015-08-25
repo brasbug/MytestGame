eb40938f2c2fdac0aaee25414a6571a70e0b2569
Jack <yu@whosv.com>
2015-08-25 15:25:23 +0800

    first

---
+++ FirstGame/FirstGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FirstGame
+//
+//  Created by Jack on 15/8/25.
+//  Copyright (c) 2015年 Jack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
